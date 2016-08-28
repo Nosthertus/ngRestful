d7fe0a1e0ba1505bb0252e18cba871a7456018c0
Nosthertus <nosthertus@gmail.com>
2016-08-28 05:07:31 -0400

    ADDED: Service verbs
    *Added common verbs to the service (POST, PUT, DELETE, GET)

--- src/service.js
+++ src/service.js
@@ -1,8 +1,66 @@
 ngRestful.service("$restful", ["$http", function($http){
-	this.get = function(url, headers){
+	/**
+	 * Performs a GET request to the host
+	 * 
+	 * @param  {String}  url     The url path where to send the request
+	 * @param  {Object}  headers The headers options to set in the request
+	 * @return {Promise}         The response from the host
+	 */
+	this.get = function(url[string], headers){
 		return $http({
 			method: "GET",
 			url: url,
+			headers: headers || {}
+		});
+	};
+
+	/**
+	 * Performs a POST request to the host
+	 * 
+	 * @param  {String}  url     The url path where to send the request
+	 * @param  {Object}  data    The data to send along the request
+	 * @param  {Object}  headers The headers options to set in the request
+	 * @return {Promise}         The response from the host
+	 */
+	this.post = function(url, data, headers){
+		return $http({
+			method: "POST",
+			url: url,
+			data: data,
+			headers: headers || {}
+		});
+	};
+
+	/**
+	 * Performs a PUT request to the host
+	 * 
+	 * @param  {String}  url     The url path where to send the request
+	 * @param  {Object}  data    The data to send along the request
+	 * @param  {Object}  headers The headers options to set in the request
+	 * @return {Promise}         The response from the host
+	 */
+	this.put = function(url, data, headers){
+		return $http({
+			method: "PUT",
+			url: url,
+			data: data,
+			headers: headers || {}
+		});
+	};
+
+	/**
+	 * Performs a DELETE request to the host
+	 * 
+	 * @param  {String}  url     The url path where to send the request
+	 * @param  {Object}  data    The data to send along the request
+	 * @param  {Object}  headers The headers options to set in the request
+	 * @return {Promise}         The response from the host
+	 */
+	this.delete = function(url, data, headers){
+		return $http({
+			method: "DELETE",
+			url: url,
+			data: data,
 			headers: headers || {}
 		});
 	};
