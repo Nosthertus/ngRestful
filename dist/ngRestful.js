0ea8a1e02ed2bb5952730df0b44a5e2514d4023b
Nosthertus <nosthertus@gmail.com>
2016-08-29 04:05:00 -0400

    ADDED: Resource parameter function
    *Added a function to set uri parameters with an object map of parameters

--- dist/ngRestful.js
+++ dist/ngRestful.js
@@ -29,14 +29,30 @@ ngRestful.factory("$resource", ["$restful", function($restful){
 	 * @return {Promise}      The response from the host
 	 */
 	resource.prototype.fetch = function(path){
+		if(Object.prototype.toString.call(path) == "[object Array]"){
+			path = setParams(path[0], path[1])
+		}
+
 		var http = [this.$url, path].join("/");
 
 		return $restful.get(http, this.headers);
 	};
 
-	resource.prototype.getContext = function() {
-		return this;
-	};
+	/**
+	 * Sets uri parameters into the uri string
+	 * where key object is the parameter name and
+	 * value is the value to replace
+	 * 
+	 * @param {String} uri    The uri with parameters to replace
+	 * @param {Object} params The map of parameters to set into uri
+	 */
+	function setParams(uri, params){
+		for(p in params){
+			uri = uri.replace(":" + p, params[p]);
+		}
+
+		return uri;
+	}
 
 	return resource;
 }]);
