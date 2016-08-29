0d5656bec3423789422517f79bfa27b9e4fe8980
Nosthertus <nosthertus@gmail.com>
2016-08-29 04:54:52 -0400

    ADDED: Gulp prettify
    *Added module for indenting the build code

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp       = require("gulp");
 var concat     = require("gulp-concat-util");
 var wrap       = require("gulp-wrap");
+var prettify   = require("gulp-jsbeautifier");
 
 gulp.task("js", () => {
 	gulp.src("src/*.js")
 		.pipe(concat("ngRestful.js", {sep: "\n\n"}))
 		.pipe(wrap("(function(){\n<%= contents %>\n})();"))
+		.pipe(prettify({
+			indent_char: "\t",
+			indent_size: 1
+		}))
 		.pipe(gulp.dest("./dist/"));
 });
 
