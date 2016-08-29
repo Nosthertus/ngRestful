c029cafd555098279c10d6e6ed0382e4e29fb8a7
Nosthertus <nosthertus@gmail.com>
2016-08-29 00:35:41 -0400

    FIX: watching sub-folders
    *Fixed error that was not watching sub-folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var concat     = require("gulp-concat-util");
 var wrap       = require("gulp-wrap");
 
 gulp.task("js", () => {
-	gulp.src("src/*.js")
+	gulp.src("src/**/*.js")
 		.pipe(concat("ngRestful.js", {sep: "\n\n"}))
 		.pipe(wrap("(function(){\n<%= contents %>\n})();"))
 		.pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("watch", () => {
-	gulp.watch("src/*.js", ["js"]);
+	gulp.watch("src/**/*.js", ["js"]);
 });
